8a9c60b6f25ba5b5ec72f708d6303043d1bbbc74
ballack <rodrigue-dev>
2024-01-17 19:18:39 +0100

    reform

--- public/contractjs/subcription.js
+++ public/contractjs/subcription.js
@@ -392,7 +392,7 @@ var subcription = function () {
         window.mxgfcontract = await new window.web3.eth.Contract(initialiseABI().StakingnmatrixAbi, initialiseABI().stakingaddress);
         var total_invest=  await window.mxgfcontract.methods.totalinvest().call();
         var total_user=  await window.mxgfcontract.methods.numberOfUsers().call();
-        $('#total_invest').text(total_invest)
+        $('#total_invest').text(convertDiv(total_invest))
         $('#total_user').text(total_user)
     };
     const getTeam= async function(){
